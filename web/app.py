9a1ecad350decfaed19fca7bd79d59adb672c9a2
tipifv <tipifv@knu.ac.kr>
2022-05-31 20:29:30 +0900

    rooms.html에서 구들 보이기

--- web/app.py
+++ web/app.py
@@ -1,14 +1,32 @@
 import flask
+import json
+
+import processraw
+
+with open("raw/lectures.json") as f:
+	bldginfo = processraw.process(json.load(f))
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def root():
-	return flask.render_template("root.html", buildings=["IT대학 11호관", "공과대학 22호관"])
+	return flask.render_template("root.html", buildings=["IT대학4호관", "IT융복합관"])
 
 @app.route("/rooms/<building>")
 def rooms(building):
-	return flask.render_template("rooms.html", building=building, rooms=["101", "201", "301"])
+	try:
+		bldg = bldginfo[building]
+	except KeyError:
+		return "건물이 없습니다"
+	floors = dict()
+	for i in bldg.rooms:
+		if i[0] not in floors:
+			floors[i[0]] = [i]
+		else:
+			floors[i[0]].append(i)
+	floors = sorted(floors.items(), key=lambda x: '/' if x[0] == 'B' else x[0], reverse=True)
+	floors = [(f, sorted(r)) for f, r in floors]
+	return flask.render_template("rooms.html", building=building, floors=floors)
 
 @app.route("/use/<building>/<room>")
 def use(building, room):
